d85b89165f92eeb6dffc5c1c7192d6676507c1b5
Ahmed Shaban <77107952+ahmedshaban022@users.noreply.github.com>
2025-11-12 13:03:36 +0200

    add more products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,7 +2,7 @@ import { Get, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ProductsService {
-    products: string[] = ['laptop', 'phone', 'tablet', 'monitor'];
+    products: string[] = ['laptop', 'phone', 'tablet', 'monitor', 'keyboard', 'mouse', 'printer'];
     getAllProducts() {
         return this.products;
     }
