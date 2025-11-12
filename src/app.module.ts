74f71c45968a71436c924f4b7b0b541def3588d4
Ahmed Shaban <77107952+ahmedshaban022@users.noreply.github.com>
2025-11-12 12:53:37 +0200

    add a product module by cli command

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/Users.Module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
