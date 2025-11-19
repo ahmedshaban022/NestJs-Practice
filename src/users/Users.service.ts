f0daba03287c5e52aecc22dd87f2278386bbabc4
Ahmed Shaban <77107952+ahmedshaban022@users.noreply.github.com>
2025-11-19 16:37:08 +0200

    add comment s on the users service

--- src/users/Users.service.ts
+++ src/users/Users.service.ts
@@ -5,20 +5,25 @@ import { Injectable } from "@nestjs/common";
 
 export class UsersService {
     users: string[] = ["ahmed", "mahmoud", "ali", "mostafa"];
+    // get all users
     getAllUsers() {
         return this.users;
     };
+    // get a single user
     getUser(userName: string) {
         return this.users.find(user => user === userName) || "user not found";
     };
+    // add a new user
     addNewUserName(userName: string) {
         this.users.push(userName);
         return this.users
     };
+    // delete a user
     deleteUserByName(userName: string) {
         this.users = this.users.filter(user => user !== userName);
         return this.users
     };
+    // update a user
     updateUser(userName: string, name: string) {
         const index = this.users.findIndex(user => user === userName);
         if (index !== -1) {
