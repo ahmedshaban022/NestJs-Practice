7bf10e40efc5f39417cf4a75f0934768e2fe7a21
Ahmed Shaban <ahmedshaban.ua007@gmail.com>
2025-12-15 19:41:38 +0200

    add a comment on the app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+// this is the controller for the app
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
